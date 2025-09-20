56bad997cd1b8b4398c9f5bd252317622cfb932b
GoeSoft <artem0322@bk.ru>
2025-09-20 19:17:34 +0300

    первый коммит: рабочая версия интернет-магазина

---
+++ shop/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Product
+
+def product_list(request):
+    """
+    Отображает список всех товаров.
+    """
+    products = Product.objects.all()
+    return render(request, 'shop/product_list.html', {'products': products})
+
+def product_detail(request, product_id):
+    """
+    Отображает детальную информацию о товаре.
+    Если товар не найден — возвращает 404.
+    """
+    product = get_object_or_404(Product, id=product_id)
+    return render(request, 'shop/product_detail.html', {'product': product})
